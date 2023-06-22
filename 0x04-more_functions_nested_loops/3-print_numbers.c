8ce17ba0376f83781d23c45f27d55762c0140a9a
Odrachael <missrachael41@gmail.com>
2023-06-22 16:37:30 +0100

    task3

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -2,14 +2,16 @@
 /**
  * print_numbers - this is the function prints numbers, from 0 to 9
  * followed by a new line
+ * return: 0
  */
+
 void print_numbers(void)
 {
-	int i;
+	char i;
 
-	for (i = 0; i < 10; i++)
+	for (i = '0'; i < '10'; i++)
 	{
-		_putchar(i + '0');
+	_putchar(i + '0');
 	}
 	_putchar('\n');
 }
