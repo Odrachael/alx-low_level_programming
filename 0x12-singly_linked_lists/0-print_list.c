bdd0c67a0b6212d6c6a2a0b9bebd0e39b62b52dd
Odrachael <missrachael41@gmail.com>
2023-07-28 00:56:36 +0100

    task 0

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -6,9 +6,9 @@
  */
 size_t print_list(const list_t *h)
 {
-	size_t i;
+	size_t nd;
 
-	i = 0;
+	nd = 0;
 	while (h != NULL)
 	{
 		if (h->str == NULL)
@@ -16,7 +16,7 @@ size_t print_list(const list_t *h)
 		else
 			printf("[%d] %s\n", h->len, h->str);
 		h = h->next;
-		i++;
+		nd++;
 	}
-	return (i);
+	return (nd);
 }
