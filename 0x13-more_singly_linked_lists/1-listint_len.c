ffdc24d3f4966bdbfe5d8e397b90fd0a6b96137e
Odrachael <missrachael41@gmail.com>
2023-08-01 10:28:59 +0100

    task 1

--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -9,7 +9,7 @@ size_t listint_len(const listint_t *h)
 	const listint_t *element_block = h;
 	size_t num_of_element = 0;
 
-	while (new_node != NULL)
+	while (element_block != NULL)
 	{
 		num_of_element += 1;
 		element_block = element_block->next;
