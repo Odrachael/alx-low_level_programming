2452055cb50b5610e829a9fb43a5e7e0f75cea3e
Odrachael <missrachael41@gmail.com>
2023-08-01 11:01:21 +0100

    task 3

--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -9,8 +9,8 @@
  */
 listint_t *add_nodeint_end(listint_t **head, const int n)
 {
-	listint_t *last_node
-	listint_t *element_node= *head
+	listint_t *last_node;
+	listint_t *element_node= *head;
 
 	last_node = malloc(sizeof(listint_t));
 	if (last_node != NULL)
